27bc7380909aa53241a2f91a3195759aa38443b8
adiarollins <Rollins.aimee.a@gmail.com>
2026-02-18 20:11:41 -0600

    p2

---
+++ HPC_SampleCodes/HPC_SampleCodes/CachingProblem.cpp
@@ -0,0 +1,110 @@
+/*
+// iostream library for input output via console/terminal
+#include <iostream>
+// thread library for threading operations
+#include <thread>
+// StopWatch class for time measurement operations
+#include "StopWatch.h"
+// std namespace for standart library functions and classes
+using namespace std;
+
+
+// Simple Threading Examples - 9
+// Multithreading Slower than Single Threading Problem
+// Use one sum and show the racing problem first!!!!
+
+
+long sum = 0;
+int numofthreads = 5;
+long length = 100000000;
+
+int* myarray = new int[length];
+long* sumeach = new long[numofthreads];
+
+void myThreadMethod(int threadid)
+{
+	StopWatch sw;
+	// Begin timing
+	sw.start();
+
+	sumeach[threadid] = 0;
+
+	for (int i = (threadid)*length / numofthreads; i < (threadid + 1) * length / numofthreads; i++)
+	{
+		sumeach[threadid] += myarray[i];
+	}
+
+
+	// Stop timing
+	sw.stop();
+
+	// Write result
+	cout << "ThreadID: " << threadid << " , Time elapsed: " << sw.elapsedTime() << endl;
+}
+
+
+int main()
+{
+	// Creating an array of a given size and assigning 1 to all elements
+	myarray = new int[length];
+	for (int i = 0; i < length; i++)
+	{
+		myarray[i] = 1;
+	}
+
+	StopWatch sw;
+
+	///////////////////////////////////////////////////////////////////
+	// SERIAL PROGRAMMING --> finding the sum of all the elements
+
+	// Begin timing
+	sw.start();
+
+	for (int i = 0; i < length; i++)
+	{
+		sum += myarray[i];
+	}
+
+	// Stop timing
+	sw.stop();
+
+	// Write result
+	cout << "SUM Serial = " << sum << endl;
+	cout << "Serial Time elapsed: " << sw.elapsedTime() << endl;
+	///////////////////////////////////////////////////////////////////
+
+
+	///////////////////////////////////////////////////////////////////
+	// PARALLEL PROGRAMMING --> finding the sum of all the elements
+
+	// Creating Thread Array
+	thread* t = new thread[numofthreads];
+
+	// Begin timing
+	sw.start();
+
+	for (int i = 0; i < numofthreads; i++)
+	{
+		t[i] = thread(myThreadMethod, i);
+	}
+
+	for (int i = 0; i < numofthreads; i++)
+	{
+		t[i].join();
+	}
+
+	// Adding all the sums coming from the threads
+	sum = 0;
+	for (int i = 0; i < numofthreads; i++)
+	{
+		sum += sumeach[i];
+	}
+
+	// Stop timing
+	sw.stop();
+
+	cout << "SUM Paralel = " << sum << endl;
+	cout << "Paralel Time elapsed: " << sw.elapsedTime() << endl;
+	///////////////////////////////////////////////////////////////////
+
+}*/
---
+++ HPC_SampleCodes/HPC_SampleCodes/ParallelIntegration_TaskParallelism.cpp
@@ -0,0 +1,96 @@
+// iostream library for input output via console/terminal
+#include <iostream>
+// thread library for threading operations
+#include <thread>
+// StopWatch class for time measurement operations
+#include "StopWatch.h"
+// std namespace for standart library functions and classes
+using namespace std;
+
+
+
+double sum = 0;
+double n = 100000; // Resolution, how many steps
+int threads;
+double a = 0, b = 10;
+
+void myThreadMethod(double start, double end)
+{
+	double localsum = 0; // every thread will have its local sum
+
+	double h = (end - start) / (n / threads);
+	for (double x = start; x < end; x += h)
+	{
+		localsum += x * x * h;
+	}
+	sum += localsum;
+}
+
+
+int main()
+{
+	StopWatch sw;
+
+	// Ask user to enter the thread number
+	cout << "How many threads you will use? >>> ";
+	cin >> threads;
+
+	// divide the domain into n parts, for example if the algorith finds the integration between
+	// 0-100, and number of threads, n is 4 --> each thread will do the integration in (100-0)/4 = 25
+	double size = (b - a) / threads;
+
+	// create a thread array
+	thread* t = new thread[threads];
+
+	//////////////////////////////////////////////////////////////////////////////////
+	// PARALEL VERSION
+	//////////////////////////////////////////////////////////////////////////////////
+
+
+	// Begin timing
+	sw.start();
+
+	for (int i = 0; i < threads; i++)
+	{
+		t[i] = thread(myThreadMethod, i * size, (i + 1) * size);
+	}
+
+	for (int i = 0; i < threads; i++)
+	{
+		t[i].join();
+	}
+
+	// Stop timing
+	sw.stop();
+
+	// The final integration
+	cout << "Parallel Result = " << sum << endl;
+	cout << "Parallel Time elapsed: " << sw.elapsedTime() << endl;
+
+
+
+
+
+	//////////////////////////////////////////////////////////////////////////////////
+	// SERIAL VERSION
+	//////////////////////////////////////////////////////////////////////////////////
+	// Check the serial one
+	double h = (b - a) / n;
+	double sum2 = 0, x;
+
+	// Begin timing
+	sw.start();
+
+	for (x = a; x <= b - h; x += h)
+	{
+		sum2 += (x * x) * h;
+	}
+
+	// Stop timing
+	sw.stop();
+
+	cout << "Serial Result = " << sum2 << endl;
+	cout << "Serial Time elapsed: " << sw.elapsedTime() << endl;
+
+
+}
