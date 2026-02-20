596e490d0f6777b05855ad1964764f3f3f509449
adiarollins <81581692+adiarollins@users.noreply.github.com>
2026-02-19 23:36:53 -0600

    Assignment 2 done

--- Assignment2/Assignment2/Assignment2.cpp
+++ Assignment2/Assignment2/Assignment2.cpp
@@ -1,20 +1,69 @@
 // Assignment2.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
+// iostream library for input output via console/terminal  
 
-#include <iostream>
 
-int main()
-{
-    std::cout << "This is a change!\n";
+#include  <iostream>;    
+
+#include  <thread>; 
+
+#include "StopWatch (1).h" 
+
+#include <mutex>;  
+
+using namespace std;
+
+
+
+int a[] = { 4, 3, 1, 6, 8, 9, 100, 23, 13, 44, 14, 67, 89, 34, 23, 69, 100, 65, 32, 33, 4 ,5 ,6 ,18 };
+
+int b[] = { 1, 7, 8, 4, 9, 12, 10, 3, 1, 14, 7, 7, 9, 3, 3, 6, 11, 165, 2, 1, 14, 15, 8, 33 };
+
+int c[24];
+
+int sum = 0;
+
+
+
+mutex mtx;
+
+
+
+void myThreadMethod(int thread_id, int N, int p) {
+
+    for (int i = thread_id * N / p; i < (thread_id + 1) * N / p; i++) {
+
+        c[i] = a[i] + b[i];
+
+    }
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+int main() {
+
+    int p = 8;
+    int N = 24;
+    int size = N / p;
+
+    thread* t = new thread[p];
+
+    for (int id = 0; id < p; id++) {
+        t[id] = thread(myThreadMethod, id, N, p);
+    }
+
+    for (int i = 0; i < p; i++)
+    {
+
+        t[i].join();
+    }
+
+    cout << "c[] : ";
+    for (int i = 0; i < N; i++) {
+        cout << c[i] << " ";
+    }
+    cout << endl;
+
+
+}
